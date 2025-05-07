33dbf78350aa86b1b814140b6d196c72a80eb67b
ViosSixth <risimac@outlook.com>
2025-05-07 10:35:18 +0200

    re-take week 7

--- answers.sql
+++ answers.sql
@@ -1,24 +1,44 @@
 ---Answer for Question 1 ----
-CREATE TABLE customers(
-    OrderID VARCHAR(10) PRIMARY KEY,
-    customerName VARCHAR(100)
-);
 
-CREATE TABLE products(
-    productID VARCHAR(100) PRIMARY KEY,
-    Products VARCHAR(100),
-    FOREIGN KEY (OrderID) REFERENCES customers(OrderID) 
+DROP TABLE ProductDetail;
+CREATE TABLE ProductDetail(
+    OrderID int,
+    customerName VARCHAR(100),
+    products VARCHAR(100)
 );
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
 
 ---Answer for Question 2 ----
-CREATE TABLE customers(
-    OrderID VARCHAR(10) PRIMARY KEY,
+CREATE TABLE Orders(
+    OrderID INT PRIMARY KEY,
     customerName VARCHAR(100)
 );
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
-CREATE TABLE products(
-    productID VARCHAR(100) PRIMARY KEY,
-    Products VARCHAR(100),
-    Quantity VARCHAR(100),
-    FOREIGN KEY (OrderID) REFERENCES customers(OrderID) 
+CREATE TABLE Product(
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID,Product),
+    FOREIGN KEY (OrderID) REFERENCES orders(OrderID) 
 );
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
